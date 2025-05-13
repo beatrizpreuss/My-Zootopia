cecd9ffb93c28791917328e2e36ff9a68144fb24
Beatriz Preuss <beatrizpreuss@Beatrizs-MacBook-Air.local>
2025-05-13 16:20:52 +0200

    Bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,19 +7,42 @@ def load_data(file_path):
     return json.load(handle)
 
 
+def get_html(html_file):
+    html = open(html_file)
+    return html.read()
+
+
 def get_info():
     animals_data = load_data('animals_data.json')
+    output = ""
     for animal in animals_data:
         try:
-            name = animal["name"]
-            diet = animal["characteristics"]["diet"]
-            location = animal["locations"][0]
-            type = animal["characteristics"]["type"]
+            output += (f"Name: {animal["name"]}\n"
+                       f"Diet: {animal["characteristics"]["diet"]}\n"
+                       f"Location: {animal["locations"][0]}\n"
+                       f"Type: {animal["characteristics"]["type"]}\n"
+                       f"\n")
         except KeyError:
             continue
-        print(f"Name: {name}\n"
-              f"Diet: {diet}\n"
-              f"Location: {location}\n"
-              f"Type: {type}\n")
+    return output
 
-get_info()
+
+def replace_info():
+    original_html = get_html("animals_template.html")
+    animals_info = get_info()
+    new_html = original_html.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+    return new_html
+
+
+def write_html():
+    info = replace_info()
+    with open("animals.html", "a") as new_html:
+        new_html.write(info)
+
+
+def main():
+    write_html()
+
+
+if __name__ == "__main__":
+    main()
